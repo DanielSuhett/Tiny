53651df71ba699c251b53639966cfe908dd03a24
Daniel Suhett <danysuhett@hotmail.com>
2023-12-03 17:10:30 -0300

    change insertOne interface

--- src/domain/repositories/urlRepository.interface.ts
+++ src/domain/repositories/urlRepository.interface.ts
@@ -1,7 +1,8 @@
-import { Url } from '../model/url';
+import { OutputCreateUrlDto } from 'src/usecases/url/create/create.url.dto';
+import { Url } from 'src/domain/model/url';
 
 export interface IUrlRepository {
-  insertOne(url: Url): Promise<boolean>;
+  insertOne(url: Url): Promise<OutputCreateUrlDto>;
   findOne(id: number): Promise<Url>;
   findMany(query: string): Promise<Url[]>;
   deleteOne(id: number): Promise<boolean>;
