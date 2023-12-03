22760cea7aa435158f25a4edf12844c32f2cb413
Daniel Suhett <danysuhett@hotmail.com>
2023-12-03 10:43:59 -0300

    create tiny url usecases

--- src/usecases/createTinyUrl.usecases.ts
+++ src/usecases/createTinyUrl.usecases.ts
@@ -2,13 +2,13 @@ import { IUrlService } from 'src/domain/adapters/url.interface';
 import { Url } from 'src/domain/model/url';
 import { IUrlRepository } from 'src/domain/repositories/urlRepository.interface';
 
-export class createTinyUrl {
+export class createTinyUrlUseCase {
   constructor(
     private readonly urlRepository: IUrlRepository,
     private readonly urlService: IUrlService,
   ) {}
 
-  async execute(owner: number, destiny: string, length = 5): Promise<any> {
+  async execute(owner: number, destiny: string, length = 5): Promise<boolean> {
     const url = new Url();
     const now = new Date();
 
