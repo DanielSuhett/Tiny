8ceb81e735c3bde4cbb74b036d9725a7b7f196dc
Daniel Suhett <danysuhett@hotmail.com>
2023-12-09 11:17:22 -0300

    init with cache and throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ControllersModule } from 'src/infrastructure/controllers/controllers.module';
 import { Url } from './domain/entities/url.entities';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -17,6 +25,9 @@ import { Url } from './domain/entities/url.entities';
       autoLoadEntities: true,
     }),
     ControllersModule,
+    CacheModule.register({
+      isGlobal: true,
+    }),
   ],
   controllers: [],
   providers: [],
